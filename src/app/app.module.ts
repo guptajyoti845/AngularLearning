9e3eb7446533473a09fc6eb59368442f96684463
Jyoti Gupta <Gupta_Jyoti@cat.com>
2021-03-28 20:37:03 +0530

    add routing to all user and server component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,33 @@ import {DropdownDirective} from './shared/dropdown.directive';
 import {UserformComponent} from './userform/userform.component';
 import {ActiveUsersComponent} from './active-users/active-users.component';
 import {InactiveUsersComponent} from './inactive-users/inactive-users.component';
+import {HomeComponent} from './home/home.component';
+import {UserComponent} from './users/user/user.component';
+import {ServerComponent} from './servers/server/server.component';
+import {ServersComponent} from './servers/servers.component';
+import {UsersComponent} from './users/users.component';
+import {EditServerComponent} from './servers/edit-server/edit-server.component';
+import {ServersService} from './servers/servers.service';
+import {RouterModule, Routes} from '@angular/router';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {
+    path: 'users', component: UsersComponent, children: [
+      {path: ':id/:name', component: UserComponent},
+    ]
+  },
+
+  {
+    path: 'servers', component: ServersComponent, children: [
+      {path: ':id', component: ServerComponent},
+      {path: ':id/edit', component: EditServerComponent},
+    ]
+  },
+  {path: 'not-found', component: PageNotFoundComponent},
+  {path: '**', redirectTo: 'not-found'},
+];
 
 @NgModule({
   declarations: [
@@ -28,14 +55,22 @@ import {InactiveUsersComponent} from './inactive-users/inactive-users.component'
     DropdownDirective,
     UserformComponent,
     ActiveUsersComponent,
-    InactiveUsersComponent
+    InactiveUsersComponent,
+    HomeComponent,
+    UsersComponent,
+    ServersComponent,
+    UserComponent,
+    EditServerComponent,
+    ServerComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
   ],
-  providers: [],
+  providers: [ServersService],
   bootstrap: [AppComponent]
 })
 export class AppModule {
